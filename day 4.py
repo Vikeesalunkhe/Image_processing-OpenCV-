336e95029d514e6811f2b074d078d2b7b3e1937b
Vikeesalunkhe <vikeesalunkhe2002@gmail.com>
2023-11-12 16:04:05 +0530

    read_and_show_img

--- day 4.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import numpy as np
-
-img=cv2.imread("pilu.jpg")
-#cv2.imshow("original" ,img)
-gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-cv2.imshow("Gray_image" ,gray)
-           
-t
---
+++ read_and_show_img.py
@@ -0,0 +1,5 @@
+import cv2
+
+img = cv2.imread("apple_img.jpg")
+cv2.imshow("Original_img",img)
+cv2.waitKey(0)
